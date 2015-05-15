b1c24793a684a592a8304f00f6f53fbff0e580d8
jakden <jakd.dennis@gmail.com>
2015-05-15 00:07:00 -0400

    added tweet grouping
    all tweets now append to a list that has been utf-8 encoded for
    troublesome characters.

--- twitter_lang_search.py
+++ twitter_lang_search.py
@@ -4,7 +4,7 @@ import collections
 from collections import Counter
 
 
-tweets_data_path = 'C:/Python34/Scripts/Python_TwitterAPIMostPopularLanguage/twitterdata.txt'
+tweets_data_path = 'C:/Python34/Scripts/twitter/twitterdata.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path, "r")
@@ -14,13 +14,38 @@ for line in tweets_file:
         tweets_data.append(tweet)
     except:
         continue
+
+while_max = len(tweets_data)
+while_max = int(while_max)
+
+y = 0
+full_text = []
+failed_text = 0
+
+while y != 100: #whileloop to run through all tweets in the file
+  y += 1
+  try:
+    textoftweet = tweets_data[y]['text']
+    textoftweet = textoftweet.encode("utf-8", errors='ignore')
+    full_text.append(textoftweet)
+  except:
+      print('failed to decypher and append')
+
+    
+full_text.append(textoftweet)
+#full_encoded_text = full_text.encode("utf-8", errors='ignore')
+print(full_text)
+
+
+
+
+
 while_max = len(tweets_data)
 while_max = int(while_max)
 print(type(while_max))
 print(while_max) #prints max number of tweets in the list
 
 #tried twitter_param = input('which twitter parameter would you like to search for?') but will not work for other parms
-
 x = 0
 full_dict = []
 failed_dict = 0
