9a5cf288b75ec097342eefd04f071460e3288766
jakden <jakd.dennis@gmail.com>
2015-05-15 16:57:13 -0400

    added ability to search for a word
    can now search for a word, within the full list.  need to make the
    search filter each list item one by one rather than a string, also the
    ability to search for portions of a string.

--- twitter_search.py
+++ twitter_search.py
@@ -2,9 +2,10 @@
 import json
 import collections
 from collections import Counter
+import re
 
 
-tweets_data_path = 'C:/Python34/Scripts/twitter/twitterdata.txt'
+tweets_data_path = 'C:/Python34/Scripts/Python_TwitterAPISearch/twitterdata.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path, "r")
@@ -15,56 +16,61 @@ for line in tweets_file:
     except:
         continue
 
+print('param options are:\n' 'text - gives you all text from all tweets in .txt\n'
+      'lang - gives you a count for how many tweets appear in different languages from tweets in .txt')
+#
+search_param = input('which search param do you want? ')
+
 while_max = len(tweets_data)
 while_max = int(while_max)
 
 y = 0
 full_text = []
 failed_text = 0
+failed_lang = 0
 
-while y != 100: #whileloop to run through all tweets in the file
+while y != while_max: #whileloop to run through all tweets in the file
   y += 1
   try:
-    textoftweet = tweets_data[y]['text']
-    textoftweet = textoftweet.encode("utf-8", errors='ignore')
+    textoftweet = tweets_data[y][search_param]
+   # textoftweet = textoftweet.encode("utf-8", errors='ignore')
     full_text.append(textoftweet)
   except:
+    if search_param == 'text':
       print('failed to decypher and append')
+    if search_param ==  'lang':
+      failed_lang += 1  
 
-    
-full_text.append(textoftweet)
-#full_encoded_text = full_text.encode("utf-8", errors='ignore')
-print(full_text)
-
-
-
-
-
-while_max = len(tweets_data)
-while_max = int(while_max)
-print(type(while_max))
-print(while_max) #prints max number of tweets in the list
-
-#tried twitter_param = input('which twitter parameter would you like to search for?') but will not work for other parms
-x = 0
-full_dict = []
-failed_dict = 0
-
-while x != while_max: #whileloop to run through all tweets in the file
-  x += 1
-  try:
-    dict_1 = tweets_data[x]['lang'] #creates an object to be appended to a list of all languages
-    
-    full_dict.append(dict_1)
-  except: #exception for no language given
-    failed_dict += 1
-    
-    
-print( failed_dict, 'tweets do not have an assigned language')    
-print(Counter(full_dict)) #counts and displays the number of tweets from each language
+#if search_param == 'id':
+ # print(full_text)
+#if search_param == 'text':  #removed this  ---- .encode("utf-8", errors='ignore') needs to be done at the end of your search
+  #print(full_text)
+if search_param == 'lang':
+  print(Counter(full_text))
+  for i in Counter(full_text):
+    print(i)
+  print( failed_lang, 'tweets do not have an assigned language')  
 
 
+#Search full text for a specific word
+#display those tweets with the word associated with them
 
+search_tweets = (input('What word would you like to search for? '))
+#full_list -> search_tweets is the word
+if any(search_tweets in s for s in full_text):  
+  print('There are results')
+search_tweets = ([s for s in full_text if search_tweets in s])
+#search_tweets is a list
+#SPLIT ALL OF THESE TO DISPLAY IN A ROW
+#NEEDS TO BE DONE BY RUNNING EACH ITEM THROUGH STEPS RATHER THAN WHOLE LIST
+
+search_tweet_string = ''.join(search_tweets)
+print(type(search_tweet_string))
+print(search_tweet_string.encode("utf-8", errors='ignore'))
+  
+
+#split list eash word
+#contains attribute
 
 
 
