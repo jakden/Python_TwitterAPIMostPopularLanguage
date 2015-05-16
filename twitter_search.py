6cf3f337102647714f11bc39180223f16c41a76d
jakden <jakd.dennis@gmail.com>
2015-05-16 15:32:26 -0400

    added display of user_id
    user_id displays before text of tweet, exceptions still display and
    exception count is not matching extra displayed user_id.

--- twitter_search.py
+++ twitter_search.py
@@ -9,6 +9,7 @@ tweets_data_path = 'C:/Python34/Scripts/Python_TwitterAPISearch/twitterdata.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path, "r")
+
 for line in tweets_file:
     try:
         tweet = json.loads(line)
@@ -16,10 +17,15 @@ for line in tweets_file:
     except:
         continue
 
+
+
 print('param options are:\n' 'text - gives you all text from all tweets in .txt\n'
       'lang - gives you a count for how many tweets appear in different languages from tweets in .txt')
 #
 search_param = input('which search param do you want? ')
+
+if search_param == 'text':
+  search_tweets = (input('What word would you like to search for? '))
 
 while_max = len(tweets_data)
 while_max = int(while_max)
@@ -29,53 +35,45 @@ full_text = []
 failed_text = 0
 failed_lang = 0
 
+id_of_tweet = []
+
 while y != while_max: #whileloop to run through all tweets in the file
   y += 1
+
   try:
-    textoftweet = tweets_data[y][search_param]
-   # textoftweet = textoftweet.encode("utf-8", errors='ignore')
-    full_text.append(textoftweet)
+    text_of_tweet = tweets_data[y][search_param]
+    id_of_tweet = tweets_data[y]['id_str']
+    full_text.append(text_of_tweet)
+
+    if search_tweets in text_of_tweet:
+      #print(aa)
+      #print(bb)
+      print(id_of_tweet)
+      print(text_of_tweet)
+      print('')
+      
   except:
     if search_param == 'text':
-      print('failed to decypher and append')
+      failed_text += 1
+
     if search_param ==  'lang':
       failed_lang += 1  
 
-#if search_param == 'id':
- # print(full_text)
-#if search_param == 'text':  #removed this  ---- .encode("utf-8", errors='ignore') needs to be done at the end of your search
-  #print(full_text)
 if search_param == 'lang':
   print(Counter(full_text))
-  for i in Counter(full_text):
-    print(i)
-  print( failed_lang, 'tweets do not have an assigned language')  
-
-
-#Search full text for a specific word
-#display those tweets with the word associated with them
-
-search_tweets = (input('What word would you like to search for? '))
-#full_list -> search_tweets is the word
-if any(search_tweets in s for s in full_text):  
-  print('There are results')
-search_tweets = ([s for s in full_text if search_tweets in s])
-#search_tweets is a list
-#SPLIT ALL OF THESE TO DISPLAY IN A ROW
-#NEEDS TO BE DONE BY RUNNING EACH ITEM THROUGH STEPS RATHER THAN WHOLE LIST
-
-search_tweet_string = ''.join(search_tweets)
-print(type(search_tweet_string))
-print(search_tweet_string.encode("utf-8", errors='ignore'))
-  
-
-#split list eash word
-#contains attribute
+  print(failed_lang, 'tweets do not have an assigned language') 
+if search_param == 'text':
+  print(failed_text, 'failed to decypher and append')
+#.encode("utf-8", errors='ignore')
+#add sender of tweet
+#link to their page???
 
 
 
 
 
 
-
-
+#ids = []
+#for tweet in tweets:
+    #if 'id_str' in tweet:
+        #ids.append(tweet['id_str'])
